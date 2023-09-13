a113ee35c014aaae5900473af90aeafec2466751
yuzchen7 <yuzchen7@gmail.com>
2023-09-13 19:43:03 -0400

    init: user table model

---
+++ db/models/user.js
@@ -0,0 +1,84 @@
+const crypto = require("crypto");
+const { Model, DataTypes } = require("sequelize");
+const db = require("../db");
+
+class User extends Model {
+   static async generateSalt() {
+      return crypto.randomBytes(16).toString("base64");
+   }
+
+   static async encryptPassword(password, salt) {
+      return crypto
+         .createHash("RSA-SHA256")
+         .update(password)
+         .update(salt)
+         .digest("hex");
+   }
+
+   async validatePassword(targetPassword) {
+      return (
+         (await User.encryptPassword(targetPassword, this.salt)) === this.password
+      );
+   }
+}
+
+User.init(
+   {
+      firstName: {
+         type: DataTypes.STRING,
+         allowNull: true,
+      },
+
+      lastname: {
+         type: DataTypes.STRING,
+         allowNull: true
+      },
+
+      middleName: {
+         type: DataTypes.STRING,
+         allowNull: true
+      },
+
+      username: {
+         type: DataTypes.STRING,
+         require: true
+      },
+
+      password: {
+         type: DataTypes.STRING,
+      },
+
+      salt: {
+         type: DataTypes.STRING,
+      }
+   },
+   {
+      timestamps: false,
+      sequelize: db,
+      modelName: "user",
+      hooks: {
+         beforeSave: async(user) => {
+            if (user.changed("password")) {
+               user.salt = await User.generateSalt();
+               user.password = await User.encryptPassword(
+                  user.password,
+                  user.salt
+               );
+            }
+         },
+         beforeBulkCreate: async(users) => {
+            for (let user of users) {
+               if (user.changed("password")) {
+                  user.salt = await User.generateSalt();
+                  user.password = await User.encryptPassword(
+                     user.password,
+                     user.salt
+                  );
+               }
+            }
+         }
+      }
+   }
+);
+
+module.exports = User;
