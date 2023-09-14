2b3793a4c5c6ac7d3f69ad5f361d16cfa0410103
yuzchen7 <yuzchen7@gmail.com>
2023-09-14 14:16:00 -0400

    test: add firend request dump data for testing

--- data/index.js
+++ data/index.js
@@ -1,6 +1,7 @@
 const UserSeed = require('./user');
 const FriendListSeed = require('./FriendList');
+const FriendRequestSeed = require('./FriendRequest');
 
 module.exports = {
-   UserSeed, FriendListSeed
+   UserSeed, FriendListSeed, FriendRequestSeed
 };
--- seed.js
+++ seed.js
@@ -1,10 +1,11 @@
 const db = require("./db");
-const { User, friend_list } = require("./db/models");
-const { UserSeed, FriendListSeed } = require("./data");
+const { User, friend_list, friend_request } = require("./db/models");
+const { UserSeed, FriendListSeed, FriendRequestSeed } = require("./data");
 
 const seed = async () => {
   await User.bulkCreate(UserSeed);
   await friend_list.bulkCreate(FriendListSeed);
+  await friend_request.bulkCreate(FriendRequestSeed);
 };
 
 seed().then(() => process.exit());
