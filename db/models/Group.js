19811b803e36c99b0eccb70799951a8f7df82d0d
yuzchen7 <yuzchen7@gmail.com>
2023-09-16 12:52:07 -0400

    init: group table

---
+++ db/models/Group.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require('sequelize');
+const db = require("../db");
+
+const group = db.define("group", {
+   id : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      unique : true,
+      primaryKey : true,
+      autoIncrement : true,
+      validate : {
+         isInt : true,
+         notEmpty : true
+      }
+   },
+
+   name : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue : "unnamed",
+      validate: {
+         notEmpty: true
+      }
+   },
+
+   meet_date : {
+      type: DataTypes.STRING
+   },
+
+   meet_time : {
+      type: DataTypes.STRING
+   },
+
+   address : {
+      type: DataTypes.STRING
+   },
+
+   state : {
+      type: DataTypes.STRING
+   },
+
+   zipcode : {
+      type: DataTypes.STRING
+   }
+
+}, {
+   timestamps : false,
+});
+
+module.exports = group;
