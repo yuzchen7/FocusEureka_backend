f3a17b3728c151a9736366e32183e25ce9e6d927
yuzchen7 <yuzchen7@gmail.com>
2023-09-16 14:04:33 -0400

    init: setup group request data

---
+++ data/GroupRequest.js
@@ -0,0 +1,16 @@
+const GroupRequestSeed = [
+   {
+      request_id : 4,
+      group_id : 1
+   },
+   {
+      request_id : 2,
+      group_id : 2
+   },
+   {
+      request_id : 4,
+      group_id : 1
+   },
+];
+
+module.exports = GroupRequestSeed;
