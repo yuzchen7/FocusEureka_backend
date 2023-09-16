f6fb2a1d5187aecc77c5583b88ea8568066f4e86
yuzchen7 <yuzchen7@gmail.com>
2023-09-16 14:04:08 -0400

    init: set up group data

---
+++ data/Group.js
@@ -0,0 +1,25 @@
+const GroupSeed = [
+   {
+      name : 'gaintLab',
+      ownerid : 1,
+      meet_date : '2024-01-01',
+      meet_time : '10:00am',
+      address :  "2900 Bedford Ave",
+      city : "Brooklyn",
+      state : "New York",
+      zipcode : "11210"
+   },
+
+   {
+      name : 'BC bridge',
+      ownerid : 3,
+      meet_date : '2023-12-06',
+      meet_time : '13:00pm',
+      address :  "",
+      city : "Brooklyn",
+      state : "New York",
+      zipcode : "10038"
+   }
+];
+
+module.exports = GroupSeed;
