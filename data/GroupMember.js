b4f7d386fbdffc06ced2a06d6f02110cc933fe41
yuzchen7 <yuzchen7@gmail.com>
2023-09-16 14:04:26 -0400

    init: setup group members data

---
+++ data/GroupMember.js
@@ -0,0 +1,20 @@
+const GroupMemberSeed = [
+   {
+      group_id : 1,
+      member_id : 3
+   },
+   {
+      group_id : 2,
+      member_id : 1
+   },
+   {
+      group_id : 2,
+      member_id : 2
+   },
+   {
+      group_id : 2,
+      member_id : 5
+   },
+];
+
+module.exports = GroupMemberSeed;
