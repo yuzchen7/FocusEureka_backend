ed8942b2930accf6039da699579e724b5427888c
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-18 21:27:19 -0400

    implemented friendlist endpoint

--- api/users.js
+++ api/users.js
@@ -1,39 +1,39 @@
 const router = require('express').Router();
-const {User,friend_list} = require("../db/models");
+const { User, friend_list } = require("../db/models");
 
 router.get("/", async (req, res, next) => {
-    try {
-      const allUser = await User.findAll({order:[['id','ASC']]});
-  
-      allUser
-        ? res.status(200).json(allUser)
-        : res.status(400).send("User List Not Found");
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const allUser = await User.findAll({ order: [['id', 'ASC']] });
+
+    allUser
+      ? res.status(200).json(allUser)
+      : res.status(400).send("User List Not Found");
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/findUser", async (req, res, next) => {
-    try {
-      const username = req.query.username;
-      console.log(username);
-      const foundUser = await User.findOne({ where: { username: username } }); 
-      foundUser
-        ? res.status(200).json(foundUser)
-        : res.status(404).send("User Not Found");
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const username = req.query.username;
+    console.log(username);
+    const foundUser = await User.findOne({ where: { username: username } });
+    foundUser
+      ? res.status(200).json(foundUser)
+      : res.status(404).send("User Not Found");
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/updateInfo", async (req, res, next) => {
-  try{
+  try {
     const username = req.body.username;
     const foundUser = await User.findOne({ where: { username: username } });
-    if(foundUser){
+    if (foundUser) {
       await foundUser.update(req.body);
       (res.status(200).json(foundUser))
-    }else{
+    } else {
       res.status(404).send("User Not Found");
     }
   } catch (error) {
@@ -42,13 +42,13 @@ router.put("/updateInfo", async (req, res, next) => {
 });
 
 router.delete("/deleteUser", async (req, res, next) => {
-  try{
+  try {
     const username = req.query.username;
     const deleteUser = await User.findOne({ where: { username: username } });
-    if(deleteUser){
+    if (deleteUser) {
       await deleteUser.destroy();
       res.status(200).send(deleteUser);
-    }else{
+    } else {
       res.status(404).send("User Not Found");
     }
   } catch (error) {
@@ -57,15 +57,23 @@ router.delete("/deleteUser", async (req, res, next) => {
 })
 
 router.get("/friendList", async (req, res, next) => {
-  try{
+  try {
     const id = req.query.id;
-    const friendList = await friend_list.findAll({include: User,
-       where:{ownerid:id}
-    });
-    friendList?
+    const friendList = await User.findAll({
+      include: [
+        {
+          model: User,
+          as: 'Friends',
+          through: friend_list,
+          foreignKey: 'ownerid',
+          otherKey: 'friendid',
+        },
+      ],
+    })
+    friendList ?
       res.status(200).json(friendList)
-    : res.status(404).send("Friend List Not Found");
-  }catch (error) {
+      : res.status(404).send("Friend List Not Found");
+  } catch (error) {
     next(error);
   }
 })
