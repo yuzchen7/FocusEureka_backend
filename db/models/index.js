f3f1c470893cc507d81c9bf1c74ccd15762c6dd4
yuzchen7 <yuzchen7@gmail.com>
2023-09-22 18:16:24 -0400

    feat: add comment table and its relationship

--- db/models/index.js
+++ db/models/index.js
@@ -8,6 +8,7 @@ const like_action = require("./LikeAction");
 const post = require("./Post");
 const ImageSet = require("./ImageSet");
 const PostLike = require("./PostLike");
+const Comment = require("./Comment");
 
 /* 
    some relationship are state in the db models js file, which is Enforcing a foreign key 
@@ -25,8 +26,15 @@ User.belongsToMany(User, {
 
 ImageSet.belongsTo(post);
 
+Comment.hasOne(Comment, {
+   as : "reply_comment",
+   foreignKey: "reply_comment_id",
+   allowNull: true,
+});
+
 module.exports = {
    User, friend_list, friend_request,
    group, group_member, group_request,
-   like_action, post, ImageSet, PostLike
+   like_action, post, ImageSet, PostLike,
+   Comment
 };
