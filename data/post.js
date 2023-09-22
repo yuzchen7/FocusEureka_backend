8bc495a74edf79b18494d35010b3455177eab817
yuzchen7 <yuzchen7@gmail.com>
2023-09-22 15:56:49 -0400

    init: post dump data

---
+++ data/post.js
@@ -0,0 +1,74 @@
+const UserSeed = [
+    {
+        title: "movie nights",
+        contents: "out door movie nights in sunset park",
+        address: "7th Avenue &, 43rd St",
+        city: "Brooklyn",
+        state: "New York",
+        zipcode: "11232",
+        start_date: "2023-10-13",
+        start_time: "18:00",
+        end_date: "2023-10-18",
+        end_time: "22:00",
+        ownerid: "1",
+        event: "true"
+    },
+    {
+        title: "Food Festival",
+        contents: "A culinary extravaganza featuring local and international cuisines.",
+        address: "123 Main Street",
+        city: "San Francisco",
+        state: "California",
+        zipcode: "94101",
+        start_date: "2023-11-05",
+        start_time: "12:00",
+        end_date: "2023-11-07",
+        end_time: "20:00",
+        ownerid: "2",
+        event: "true"
+    },    
+    {
+        title: "Music Concert",
+        contents: "Live performances by top artists from various music genres.",
+        address: "456 Music Avenue",
+        city: "Los Angeles",
+        state: "California",
+        zipcode: "90001",
+        start_date: "2023-09-30",
+        start_time: "19:30",
+        end_date: "2023-10-02",
+        end_time: "23:00",
+        ownerid: "3",
+        event: "true"
+    },
+    {
+        title: "Art Exhibition",
+        contents: "Explore a diverse collection of contemporary art from local and international artists.",
+        address: "789 Gallery Lane",
+        city: "Chicago",
+        state: "Illinois",
+        zipcode: "60601",
+        start_date: "2023-10-20",
+        start_time: "11:00",
+        end_date: "2023-10-25",
+        end_time: "18:00",
+        ownerid: "4",
+        event: "true"
+    },    
+    {
+        title: "Charity Run",
+        contents: "Join us for a 5K run to support a local charity organization.",
+        address: "321 Charity Street",
+        city: "Austin",
+        state: "Texas",
+        zipcode: "78701",
+        start_date: "2023-10-15",
+        start_time: "08:00",
+        end_date: "2023-10-15",
+        end_time: "12:00",
+        ownerid: "5",
+        event: "true"
+    },
+];
+
+module.exports = UserSeed;
