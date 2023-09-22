9af2bdb84cc568a0ec3959adce88cf24ba790789
yuzchen7 <yuzchen7@gmail.com>
2023-09-22 18:15:55 -0400

    init: add Comment table

---
+++ db/models/Comment.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+
+const user = require("./user");
+const post = require("./Post");
+
+const Comment = db.define('comment', {
+   id : {
+      type : DataTypes.INTEGER,
+      primaryKey : true,
+      autoIncrement : true,
+      unique : true,
+      allowNull : false,
+      validate : {
+         notEmpty : true
+      }
+   },
+
+   onwer_id : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+         notEmpty : true
+      },
+      references : {
+         model : user,
+         key : 'id'
+      }
+   }, 
+
+   post_id : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+         notEmpty : true
+      },
+      references : {
+         model : post,
+         key : 'id'
+      }
+   },
+
+   contents : {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+         notEmpty : true
+      }
+   },
+});
+
+module.exports = Comment;
