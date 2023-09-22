936c760c0f4efa5d854454b7df69fa2fd2b6d46d
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-22 18:42:59 -0400

    complete currentUser endpoint to retrieve friend reqeust that belongs to current user

--- api/friend_request.js
+++ api/friend_request.js
@@ -14,10 +14,11 @@ router.get("/", async (req, res, next) => {
                   attributes: ['id','first_name','last_name','middle_name','username'],
                 },
               ],
+              attributes: ['id','first_name','last_name','middle_name','username'],
               order: [['id', 'ASC']] });
         all_friend_request
             ? res.status(200).json(all_friend_request)
-            : res.status(400).send("User List Not Found");
+            : res.status(404).send("Friend Request Not Found");
     } catch (error) {
         next(error);
     }
@@ -26,7 +27,7 @@ router.get("/", async (req, res, next) => {
 router.get("/currentUser", async (req, res, next) => {
     try{
         const username = req.query.username;
-        const friend_request = await User.findAll({ 
+        const friend_request_data = await User.findAll({ 
             include: [
                 {
                   model: User,
@@ -37,9 +38,13 @@ router.get("/currentUser", async (req, res, next) => {
                   attributes: ['id','first_name','last_name','middle_name','username'],
                 },
               ],
+              attributes: ['id','first_name','last_name','middle_name','username'],
               order: [['id', 'ASC']],
               where:{username:username} 
-            });
+        });
+        friend_request_data?
+            res.status(200).json(friend_request_data)
+            :res.status(404).send("Current User's Friend Request Not Found");
     }catch(error){
         next(error);
     }
