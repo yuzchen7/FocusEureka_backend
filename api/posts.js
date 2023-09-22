9a9d5c543bafacdeaaf28b407eab152e2aa37966
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-21 20:48:53 -0400

    add post dummy data to seed file

--- api/posts.js
+++ api/posts.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 const { post } = require("../db/models");
 
+router.get("/", async (req, res, next) => {
+    try {
+      const allposts = await post.findAll({ order: [['id', 'ASC']] });
+  
+      allposts
+        ? res.status(200).json(allposts)
+        : res.status(404).send("Posts Not Found");
+    } catch (error) {
+      next(error);
+    }
+});
+
 module.exports = router;
