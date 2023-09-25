3ef1857f913714977db2fd79945ab45fd66283e8
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-24 22:44:02 -0400

    implement receiving friend request endpoint

--- api/friend_request.js
+++ api/friend_request.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { friend_request, User,friend_list } = require("../db/models");
+const db = require("../db")
 
 const user_arrtibutes_filter = ['id','first_name','last_name','middle_name','username'];
 
@@ -100,29 +101,27 @@ router.post("/acceptRequest", async (req, res, next) => {
     }
 })
 
-// router.get("/receiving", async (req, res, next) => {
-//     try{
-//         const username = req.query.username;
-//         const friend_request_data = await User.findAll({ 
-//             include: [
-//                 {
-//                   model: User,
-//                   as: 'Friends_requests',
-//                   through: friend_request,
-//                   attributes: user_arrtibutes_filter,
-//                   right: true 
-//                 },
-//               ],
-//               attributes: user_arrtibutes_filter,
-//               order: [['id', 'ASC']],
-//             //   where:{username:username} 
-//         });
-//         friend_request_data?
-//             res.status(200).json(friend_request_data)
-//             :res.status(404).send("Current User's Friend Request Not Found");
-//     }catch(error){
-//         next(error);
-//     }
-// })
+router.get("/receiving", async (req, res, next) => {
+    try{
+        const username = req.query.username;
+        const sql = `select 
+        u2.first_name as requester_firstName,
+        u2.last_name as requester_lastName,
+        u2.middle_name as requester_middleName,
+        u2.username as requester_username,
+        u1.first_name as accepter_firstName,
+        u1.last_name as accepter_lastName,
+        u1.middle_name as accepter_middleName,
+        u1.username as accepter_username
+        from friend_requests left join users as u1 on U1.id = friend_requests.targetid left join users AS u2 on u2.id = friend_requests.ownerid
+        WHERE u1.username = '${username}';`;
+        const [results, metadata] = await db.query(sql)
+        results?
+            res.status(200).json(results)
+            :res.status(404).send("Current User's Friend Request Not Found");
+    }catch(error){
+        next(error);
+    }
+})
 
 module.exports = router;
