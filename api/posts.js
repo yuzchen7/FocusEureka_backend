39708b1e49d8e814324608ebe209580431069b2b
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-27 21:53:22 -0400

    add comment to posts related endpoints

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { post,User } = require("../db/models");
 
+//retrieve all posts stored in the database
 router.get("/", async (req, res, next) => {
     console.log("get all posts triggered");
     try {
@@ -14,6 +15,7 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+//retrieve all posts of current user
 router.get("/currentUser", async (req, res, next) => {
     console.log("get current user's posts triggered");
   try{
@@ -30,6 +32,20 @@ router.get("/currentUser", async (req, res, next) => {
   }
 })
 
+// {
+//   "title":"TESTING POSTS",
+//   "contents":"FOR TESTING only",
+//   "address":"NoWhere",
+//   "city":"nowhere",
+//   "state":"state",
+//   "zipcode":"11111",
+//   "start_date":"2023-09-27",
+//   "start_time":"21:37",
+//   "end_date":"2023-09-30",
+//   "end_time":"22:00",
+//   "ownerid":"1",
+//   "event":"true"
+// }
 router.post("/createPost", async (req, res, next) => {
   try{
     const postData = req.body;
