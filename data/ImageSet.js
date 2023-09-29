bc9918742e93072e46645c999fe5387a13148d7d
Kaifeng Yu <1536771545kai@gmail.com>
2023-09-29 12:31:18 -0400

    create dummy data for imageSet

---
+++ data/ImageSet.js
@@ -0,0 +1,23 @@
+const ImageSetSeed = [
+    {
+       post_id : 1,
+       url: ["https://images.pexels.com/photos/3131971/pexels-photo-3131971.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/2507025/pexels-photo-2507025.jpeg?auto=compress&cs=tinysrgb&w=1600"]
+    },
+ 
+    {
+       post_id : 2,
+       url: ["https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/1655329/pexels-photo-1655329.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/3990348/pexels-photo-3990348.jpeg?auto=compress&cs=tinysrgb&w=1600"]
+    },
+ 
+    {
+       post_id : 3,
+       url: ["https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/1540406/pexels-photo-1540406.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/1916821/pexels-photo-1916821.jpeg?auto=compress&cs=tinysrgb&w=1600","https://images.pexels.com/photos/18424687/pexels-photo-18424687/free-photo-of-woman-with-a-microphone-on-stage.jpeg?auto=compress&cs=tinysrgb&w=1600"]
+    },
+ 
+    {
+       post_id : 4,
+       url: ["https://images.pexels.com/photos/3004909/pexels-photo-3004909.jpeg?auto=compress&cs=tinysrgb&w=1600"]
+    },
+ ];
+ 
+ module.exports = ImageSetSeed;
