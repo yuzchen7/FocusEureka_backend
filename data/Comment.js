f30c273416a0f1034ed032f805b0bca2f38b9549
yuzchen7 <yuzchen7@gmail.com>
2023-10-02 18:34:06 -0400

    init: add comment dump data to current branch from postEndpoint branch

---
+++ data/Comment.js
@@ -0,0 +1,25 @@
+const CommentSeed = [
+    {
+        onwer_id : 1,
+        post_id: 1,
+        contents: "Very excited about the event..."
+    },
+    {
+        onwer_id : 2,
+        post_id: 1,
+        contents: "cant wait to see the event...",
+        reply_comment_id:1
+    },
+    {
+        onwer_id : 2,
+        post_id: 5,
+        contents: "Very excited about the event..."
+    },
+    {
+        onwer_id : 3,
+        post_id: 1,
+        contents: "testing testing"
+    }
+ ];
+ 
+ module.exports = CommentSeed;
