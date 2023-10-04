444c864422285d883c0cf9603f254d6d69d3f764
yuzchen7 <yuzchen7@gmail.com>
2023-10-04 18:56:54 -0400

    refactor: fix sql to apply with the new f_l struct

--- api/users.js
+++ api/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, friend_list } = require("../db/models");
+const db = require("../db");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -58,21 +59,44 @@ router.delete("/deleteUser", async (req, res, next) => {
 
 router.get("/friendList", async (req, res, next) => {
   try {
-    const username = req.query.username;
-    const friendList = await User.findOne({
-      attributes: ['id','first_name','last_name','middle_name','username',],
-      include: [
-        {
-          model: User,
-          as: 'Friends',
-          through: friend_list,
-          foreignKey: 'onwerid',
-          otherKey: 'friendid',
-          attributes: ['id','first_name','last_name','middle_name','username'],
-        },
-      ],
-      where:{username:username}
-    })
+    const onwer_id = req.query.onwerid;  
+    // const friendList = await User.findOne({
+    //   attributes: ['id','first_name','last_name','middle_name','username',],
+    //   include: [
+    //     {
+    //       model: User,
+    //       as: 'Friends',
+    //       through: friend_list,
+    //       foreignKey: 'onwerid',
+    //       otherKey: 'friendid',
+    //       attributes: ['id','first_name','last_name','middle_name','username'],
+    //     },
+    //   ],
+    //   where:{id:onwer_id}
+    // })
+    const sql = `SELECT 
+                    u.id,
+                    u.first_name,
+                    u.middle_name,
+                    u.last_name,
+                    u.username
+                FROM 
+                    users AS u
+                WHERE 
+                    u.id IN (
+                      SELECT fl.friendid
+                      FROM friend_lists AS fl
+                      WHERE fl.ownerid = '${onwer_id}'
+                    )
+                or 
+                    u.id IN (
+                      SELECT fl.ownerid
+                      FROM friend_lists AS fl
+                      WHERE fl.friendid = '${onwer_id}'
+                    )
+                ;`;
+
+    const [friendList, metadata] = await db.query(sql).catch(error => console.error(error));
     friendList ?
       res.status(200).json(friendList)
       : res.status(404).send("Friend List Not Found");
