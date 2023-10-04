21dc5c37364c8b486148d16947e422d542415d08
yuzchen7 <yuzchen7@gmail.com>
2023-10-04 17:00:28 -0400

    init: schedule dump data for testing

---
+++ data/Schedule.js
@@ -0,0 +1,34 @@
+const ScheduleSeed = [
+   {
+      user_id : 1,
+      mon : false,
+      tue : false,
+      wed : false,
+   },
+
+   {
+      user_id : 2,
+      mon : false,
+      tue : false,
+      wed : false,
+   },
+
+   {
+      user_id : 3,
+      fri : false,
+   },
+
+   {
+      user_id : 4
+   },
+
+   {
+      user_id : 5
+   },
+
+   {
+      user_id : 6
+   }
+];
+
+module.exports = ScheduleSeed;
