6508deeeb1f58fef818f71c45138e08bd8be29b5
Kaifeng Yu <1536771545kai@gmail.com>
2023-10-04 17:03:43 -0400

    implement get api/comments/ to retrieve all data from comments model

--- api/comments.js
+++ api/comments.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
+const { Comment } = require("../db/models");
 
-
+router.get("/", async (req,res) => {
+    try{
+        const allComments = await Comment.findAll({where:{reply_comment_id:null}});
+        allComments
+        ?res.status(200).json(allComments)
+        :res.status(404).json("Comments not found");
+    }catch(error){
+        next(error);
+    }
+})
 module.exports = router
