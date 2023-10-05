9d11e84a75895789a4f216f79de91c40c8d9bca0
Kaifeng Yu <1536771545kai@gmail.com>
2023-10-05 13:19:01 -0400

    implement get schedule/currentUser endpoint

--- api/schedule.js
+++ api/schedule.js
@@ -3,7 +3,6 @@ const{Schedule,User} = require('../db/models');
 const user_arrtibutes_filter = ['id','first_name','last_name','middle_name','username'];
 
 router.get('/', async (req, res) => {
-    console.log("get schedule triggered");
     try{
         const allWeekdays = await Schedule.findAll({
             include:[{
@@ -14,6 +13,18 @@ router.get('/', async (req, res) => {
         allWeekdays
         ?res.status(200).json(allWeekdays)
         :res.status(404).json("404 Not Found")
+    }catch(error){
+        next(error);
+    }
+})
+
+router.get('/currentUser', async (req, res) => {
+    try{
+        const userId = req.query.userId;
+        const current_user_schdule = await Schedule.findOne({where:{id:userId}});
+        current_user_schdule
+        ?res.status(200).json(current_user_schdule)
+        :res.status(404).json("current user's schedule does not exist");
     }catch(error){
         next(error);
     }
