fa5dcde922d2c3da9bb00875029094aea59b5318
yuzchen7 <yuzchen7@gmail.com>
2023-10-05 13:17:09 -0400

    feat: impl mygroups get all groups that user own

--- api/group.js
+++ api/group.js
@@ -6,7 +6,25 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/mygroups', async (req, res, next) => {
-   
+   try {
+      const user_id = req.query.userid;
+      const result = await group.findAll({
+         where : {
+            ownerid : user_id
+         }
+      }).catch(err => {
+         res.status(400);
+         err.message = "SQL error";
+         throw err;
+      });
+
+      result ?
+         res.status(200).json(result)
+         : res.status(404).send({message: "No user group founded"});
+   } catch (err) {
+      res.status(400).send({message: err.message});
+      next(err);
+   }
 });
 
 router.get('/newgroup', async (req, res, next) => {
