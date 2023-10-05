87030cb86fec5e84958301cb34ef93851a367fad
yuzchen7 <yuzchen7@gmail.com>
2023-10-05 13:11:40 -0400

    feat: set up some of the url

--- api/group.js
+++ api/group.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
-router.use('/users', require('./users'));
+const { User, group, group_member, group_request } = require('../db/models');
 
+router.get('/', async (req, res, next) => {
 
+});
+
+router.get('/mygroups', async (req, res, next) => {
+   
+});
+
+router.get('/newgroup', async (req, res, next) => {
+   
+});
+
+router.post('/groupmember', async (req, res, next) => {
+
+});
+
+router.post('/grouprequest', async (req, res, next) => {
+
+});
+
+router.post('/acceptrequest', async (req, res, next) => {
+
+});
 
 module.exports = router;
