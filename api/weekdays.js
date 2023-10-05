7480ec638f83f376dc662dd73a6ed637c0166368
Kaifeng Yu <1536771545kai@gmail.com>
2023-10-05 12:49:05 -0400

    implement get api/weekdays endpoint

--- api/weekdays.js
+++ api/weekdays.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
+const{Weekdays} = require('../db/models');
 
-
+router.get('/', async (req, res) => {
+    try{
+        const allWeekdays = Weekdays.findAll();
+        allWeekdays
+        ?res.status(200).json(allWeekdays)
+        :res.status(404).json("404 Not Found")
+    }catch(error){
+        next(error);
+    }
+})
 module.exports = router;
