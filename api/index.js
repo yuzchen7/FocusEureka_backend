ed8786c18f61cbb9d2000e9430b5a40ae2d241be
yuzchen7 <yuzchen7@gmail.com>
2023-10-05 12:56:23 -0400

    init: set up group router

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/friend_request', require('./friend_request'));
 router.use('/comments', require('./comments'));
+router.use('/groups', require('./group'));
 
 router.use((req, res, next) => {
     const error = new Error("404 Not Found");
