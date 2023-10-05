a11e657117d5abbac21cc7f90bb707cf081128c0
Kaifeng Yu <1536771545kai@gmail.com>
2023-10-05 13:11:26 -0400

    modify get api/schedule endpoint to retrieve all user's weekly schedule

--- api/schedule.js
+++ api/schedule.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-const{Weekdays} = require('../db/models');
+const{Schedule,User} = require('../db/models');
+const user_arrtibutes_filter = ['id','first_name','last_name','middle_name','username'];
 
 router.get('/', async (req, res) => {
+    console.log("get schedule triggered");
     try{
-        const allWeekdays = Weekdays.findAll();
+        const allWeekdays = await Schedule.findAll({
+            include:[{
+                model:User,
+                attributes: user_arrtibutes_filter
+            }]
+        });
         allWeekdays
         ?res.status(200).json(allWeekdays)
         :res.status(404).json("404 Not Found")
