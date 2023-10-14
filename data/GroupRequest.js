0a251a1c7158cf5474c56acf69b8262fe334c131
yuzchen7 <yuzchen7@gmail.com>
2023-10-14 15:40:51 -0400

    fix: data relationship error

--- data/GroupRequest.js
+++ data/GroupRequest.js
@@ -1,18 +1,13 @@
 const GroupRequestSeed = [
    {
-      requester_id : 1,
-      acceptor_id : 4,
-      group_id : 1
-   },
-   {
-      requester_id : 2,
+      requester_id : 4,
       acceptor_id : 1,
-      group_id : 2
+      group_id : 1
    },
    {
-      requester_id : 1,
+      requester_id : 4,
       acceptor_id : 2,
-      group_id : 1
+      group_id : 2
    },
 ];
 
