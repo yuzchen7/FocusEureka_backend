cecd211b119f3b9edd64221c4d08ef47aa0cebf6
kaifeng yu <kaifeng.yu87@bcmail.cuny.edu>
2023-10-29 21:19:08 -0400

    add new dummy data

--- data/ImageSet.js
+++ data/ImageSet.js
@@ -18,6 +18,14 @@ const ImageSetSeed = [
        post_id : 4,
        urls: ["https://images.pexels.com/photos/3004909/pexels-photo-3004909.jpeg?auto=compress&cs=tinysrgb&w=1600"]
     },
+    {
+      post_id : 5,
+      urls:["https://media.gettyimages.com/id/503565231/photo/low-section-of-marathon-runners-competing-at-park.jpg?s=612x612&w=0&k=20&c=gFdWtGo7JDs4iIEfrQJnUjbuI-S6fblQ9IMWyRXGeeg="]
+    },
+    {
+      post_id : 6,
+      urls:["https://images.squarespace-cdn.com/content/v1/571abd61e3214001fb3b9966/1518814837468-LZXSJ9HHAUX6YEDZQMF9/entrance.jpg?format=2500w","https://images.squarespace-cdn.com/content/v1/571abd61e3214001fb3b9966/1518814402334-EDMWCIKTFR4TDOSG0SVP/IMG_1374.jpg?format=2500w"]
+    }
  ];
  
  module.exports = ImageSetSeed;
