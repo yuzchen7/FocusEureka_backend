248fd77349d337492f3254619aeb4b4c4f1d7f6c
Kaifeng Yu <1536771545kai@gmail.com>
2023-12-02 22:30:05 -0500

    attempt to deploy through render

--- db/db.js
+++ db/db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 const pg = require('pg');
 require("dotenv").config();
 
-const db = new Sequelize(
-   `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
-   {
-      logging: false,
-   }
-);
-// const db = new Sequelize(process.env.POSTGRES_URL+ "?sslmode=require",);
+// const db = new Sequelize(
+//    `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+//    {
+//       logging: false,
+//    }
+// );
+const db = new Sequelize(process.env.POSTGRES_URL+ "?sslmode=require",);
 
 db.authenticate().then(() => {
    console.log("Connection has been established successfully.");
